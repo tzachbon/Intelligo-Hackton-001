93de69d248af61e9f5091762e1c152fe788292a3
Mac <mac@ip-192-168-1-118.ec2.internal>
2020-01-03 01:00:55 +0200

    creating portal

---
+++ portal/src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider } from '@material-ui/core';
+import materialTheme from './utils/theme/material.theme';
+
+ReactDOM.render(
+    <MuiThemeProvider theme={materialTheme}>
+        <App />
+    </MuiThemeProvider>
+    , document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
